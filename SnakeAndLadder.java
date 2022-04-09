eeab462942b80cef60abdfd15e4ae2773cd36e01
firozshaik36 <firozshaik0007@gmail.com>
2022-04-09 10:46:40 +0530

    Rolling: The Die to get a Number Between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,7 +3,9 @@ public class SnakeAndLadder {
     public static void main(String [] args){
 
         int startPosition = 0;
+        int dieRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
         System.out.println("Starting Position is :" + startPosition);
+        System.out.println("Dice Roll Number :" + dieRoll);
     }
 
 }
