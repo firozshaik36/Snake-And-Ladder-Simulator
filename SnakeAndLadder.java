6ced5642f7078e959db1fa623d1aff2986b69941
firozshaik36 <firozshaik0007@gmail.com>
2022-04-09 10:38:36 +0530

    Starting: At Position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,7 +2,8 @@ public class SnakeAndLadder {
 
     public static void main(String [] args){
 
-        System.out.println("Welcome To Snake And Ladder Simulator");
+        int startPosition = 0;
+        System.out.println("Starting Position is :" + startPosition);
     }
 
 }
